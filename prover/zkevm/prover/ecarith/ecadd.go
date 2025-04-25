6ec9f7b4641c14ac1a5397dd42d9d52d66697edc
Anton Levochko <57064990+OmegaTymbJIep@users.noreply.github.com>
2025-04-25 19:57:06 +0300

    Implement "glue" for `ecarith` module over koalabear field (#3)
    ## Changes
    
    Added comprehensive implementation of flatten column functionality to handle limb data in a circuit:
    
    1. Created new `FlattenColumn` component in `common` package that:
       - Manages flattened limbs and mask columns
       - Implements projection constraints to verify data consistency
       - Supports reuse across multiple circuits to avoid duplication
    
    2. Refactored EC arithmetic modules to use shared flatten column:
       - Updated ECADD and ECMUL to use common implementation
       - Added proper column sharing between circuits
       - Improved variable naming for clarity

--- prover/zkevm/prover/ecarith/ecadd.go
+++ prover/zkevm/prover/ecarith/ecadd.go
@@ -1,16 +1,16 @@
 package ecarith
 
 import (
-	"fmt"
-
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/algebra"
-	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
-	"github.com/consensys/gnark/std/math/bitslice"
-	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/linea-monorepo/prover/protocol/dedicated/plonk"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/zkevm/prover/common"
+	"github.com/consensys/gnark/std/math/emulated"
+	"fmt"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/bitslice"
+	"github.com/consensys/gnark/std/algebra"
 )
 
 const (
@@ -21,27 +21,34 @@ const (
 	nbRowsPerEcAdd = 12
 )
 
-// EcMulIntegration integrated EC_MUL precompile call verification inside a
+// EcAdd integrated EC_ADD precompile call verification inside a
 // gnark circuit.
 type EcAdd struct {
 	*EcDataAddSource
 	AlignedGnarkData *plonk.Alignment
+
+	flattenLimbs *common.FlattenColumn
 
 	size int
 	*Limits
 }
 
 func NewEcAddZkEvm(comp *wizard.CompiledIOP, limits *Limits) *EcAdd {
+	src := &EcDataAddSource{
+		CsEcAdd: comp.Columns.GetHandle("ecdata.CIRCUIT_SELECTOR_ECADD"),
+		Index:   comp.Columns.GetHandle("ecdata.INDEX"),
+		IsData:  comp.Columns.GetHandle("ecdata.IS_ECADD_DATA"),
+		IsRes:   comp.Columns.GetHandle("ecdata.IS_ECADD_RESULT"),
+	}
+
+	for i := 0; i < common.NbFlattenColLimbs; i++ {
+		src.Limbs[i] = comp.Columns.GetHandle(ifaces.ColIDf("ecdata.LIMB_%d", i))
+	}
+
 	return newEcAdd(
 		comp,
 		limits,
-		&EcDataAddSource{
-			CsEcAdd: comp.Columns.GetHandle("ecdata.CIRCUIT_SELECTOR_ECADD"),
-			Limb:    comp.Columns.GetHandle("ecdata.LIMB"),
-			Index:   comp.Columns.GetHandle("ecdata.INDEX"),
-			IsData:  comp.Columns.GetHandle("ecdata.IS_ECADD_DATA"),
-			IsRes:   comp.Columns.GetHandle("ecdata.IS_ECADD_RESULT"),
-		},
+		src,
 		[]plonk.Option{plonk.WithRangecheck(16, 6, true)},
 	)
 }
@@ -50,11 +57,13 @@ func NewEcAddZkEvm(comp *wizard.CompiledIOP, limits *Limits) *EcAdd {
 func newEcAdd(comp *wizard.CompiledIOP, limits *Limits, src *EcDataAddSource, plonkOptions []plonk.Option) *EcAdd {
 	size := limits.sizeEcAddIntegration()
 
+	flattenLimbs := common.NewFlattenColumn(comp, src.CsEcAdd.Size(), common.NbFlattenColLimbs, "ecdata", "ECADD")
+
 	toAlign := &plonk.CircuitAlignmentInput{
 		Name:               NAME_ECADD + "_ALIGNMENT",
 		Round:              ROUND_NR,
-		DataToCircuitMask:  src.CsEcAdd,
-		DataToCircuit:      src.Limb,
+		DataToCircuitMask:  flattenLimbs.Mask,
+		DataToCircuit:      flattenLimbs.Limbs,
 		Circuit:            NewECAddCircuit(limits),
 		NbCircuitInstances: limits.NbCircuitInstances,
 		PlonkOptions:       plonkOptions,
@@ -63,14 +72,18 @@ func newEcAdd(comp *wizard.CompiledIOP, limits *Limits, src *EcDataAddSource, pl
 	res := &EcAdd{
 		EcDataAddSource:  src,
 		AlignedGnarkData: plonk.DefineAlignment(comp, toAlign),
+		flattenLimbs:     flattenLimbs,
 		size:             size,
 	}
+
+	flattenLimbs.CsFlattenProjection(comp, res.Limbs[:], res.CsEcAdd)
 
 	return res
 }
 
 // Assign assigns the data from the trace to the gnark inputs.
 func (em *EcAdd) Assign(run *wizard.ProverRuntime) {
+	em.flattenLimbs.Assign(run)
 	em.AlignedGnarkData.Assign(run)
 }
 
@@ -78,7 +91,7 @@ func (em *EcAdd) Assign(run *wizard.ProverRuntime) {
 // fetch data from the EC_DATA module from the arithmetization.
 type EcDataAddSource struct {
 	CsEcAdd ifaces.Column
-	Limb    ifaces.Column
+	Limbs   [common.NbFlattenColLimbs]ifaces.Column
 	Index   ifaces.Column
 	IsData  ifaces.Column
 	IsRes   ifaces.Column
@@ -93,20 +106,20 @@ type MultiECAddCircuit struct {
 
 type ECAddInstance struct {
 	// First input to addition
-	P_X_hi, P_X_lo frontend.Variable `gnark:",public"`
-	P_Y_hi, P_Y_lo frontend.Variable `gnark:",public"`
+	P_X_hi, P_X_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
+	P_Y_hi, P_Y_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
 
 	// Second input to addition
-	Q_X_hi, Q_X_lo frontend.Variable `gnark:",public"`
-	Q_Y_hi, Q_Y_lo frontend.Variable `gnark:",public"`
+	Q_X_hi, Q_X_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
+	Q_Y_hi, Q_Y_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
 
 	// The result of the addition. Is provided non-deterministically by the
 	// caller, we have to ensure that the result is correct.
-	R_X_hi, R_X_lo frontend.Variable `gnark:",public"`
-	R_Y_hi, R_Y_lo frontend.Variable `gnark:",public"`
+	R_X_hi, R_X_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
+	R_Y_hi, R_Y_lo [common.NbFlattenColLimbs]frontend.Variable `gnark:",public"`
 }
 
-// NewECMulCircuit creates a new circuit for verifying the EC_MUL precompile
+// NewECAddCircuit creates a new circuit for verifying the EC_MUL precompile
 // based on the defined number of inputs.
 func NewECAddCircuit(limits *Limits) *MultiECAddCircuit {
 	return &MultiECAddCircuit{
