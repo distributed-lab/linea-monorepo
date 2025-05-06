8bd8f16f84f6934dc163f928ee703a0a21db52bd
nazarevsky <nazarevsky.ua@gmail.com>
2025-05-06 19:36:34 +0300

    Fix divideBytes empty input function, add test

--- prover/zkevm/prover/ecdsa/utils.go
+++ prover/zkevm/prover/ecdsa/utils.go
@@ -3,6 +3,10 @@ package ecdsa
 // divideBytes splits the input slice into subarrays of 2 bytes each.
 // If the input slice length is odd, the last subarray will contain a single byte.
 func divideBytes(input []byte) [][]byte {
+	if len(input) == 0 {
+		return [][]byte{}
+	}
+
 	var result [][]byte
 	for i := 0; i < len(input); i += 2 {
 		end := i + 2
