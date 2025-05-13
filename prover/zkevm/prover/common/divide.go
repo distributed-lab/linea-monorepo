f7cce91c686cd93da2b0b13d3965266c70462c05
nazarevsky <nazarevsky.ua@gmail.com>
2025-05-13 14:59:06 +0300

    Change constant name, remove commented import

--- prover/zkevm/prover/common/divide.go
+++ prover/zkevm/prover/common/divide.go
@@ -1,17 +1,17 @@
 package common
 
-// LimbSize is the size of one limb in bytes
-const LimbSize = 2
+// LimbBytes is the size of one limb in bytes
+const LimbBytes = 2
 
-// DivideBytes splits the input slice into subarrays of the size defined by LimbSize.
+// DivideBytes splits the input slice into subarrays of the size defined by LimbBytes.
 func DivideBytes(input []byte) [][]byte {
 	if len(input) == 0 {
 		return [][]byte{}
 	}
 
 	var result [][]byte
-	for i := 0; i < len(input); i += LimbSize {
-		end := i + LimbSize
+	for i := 0; i < len(input); i += LimbBytes {
+		end := i + LimbBytes
 		if end > len(input) {
 			end = len(input)
 		}
