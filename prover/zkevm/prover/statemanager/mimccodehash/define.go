c51b4aa8a1e1dc0d4b728ea12dd2b865852f46d9
nazarevsky <nazarevsky.ua@gmail.com>
2025-06-20 15:21:21 +0300

    Fix arith columns for ROM and ROMLEX, remove unnecessary comments

--- prover/zkevm/prover/statemanager/mimccodehash/define.go
+++ prover/zkevm/prover/statemanager/mimccodehash/define.go
@@ -116,8 +116,6 @@ func NewModule(comp *wizard.CompiledIOP, inputs Inputs) (mh Module) {
 		mh.PrevState[i] = comp.InsertCommit(inputs.Round, ifaces.ColIDf("%s_%d", MIMC_CODE_HASH_PREV_STATE_NAME, i), inputs.Size)
 		mh.NewState[i] = comp.InsertCommit(inputs.Round, ifaces.ColIDf("%s_%d", MIMC_CODE_HASH_NEW_STATE_NAME, i), inputs.Size)
 
-		// TODO: recheck this constraint because originally, it will pass
-		// if either HashHi or HashLo corresponds to the empty keccak part
 		comp.InsertGlobal(
 			0,
 			ifaces.QueryIDf("MIMC_CODE_HASH_CPT_IF_FOR_CONSISTENCY_%d", i),
@@ -154,6 +152,7 @@ func NewModule(comp *wizard.CompiledIOP, inputs Inputs) (mh Module) {
 //	11. All columns are zero in the inactive area
 func (mh *Module) checkConsistency(comp *wizard.CompiledIOP) {
 
+	// TODO: fix MiMC query
 	// NewState = MiMC(PrevState, Limb)
 	//comp.InsertMiMC(mh.inputs.Round, mh.qname("MiMC_CODE_HASH"), mh.Limb, mh.PrevState, mh.NewState, nil)
 
