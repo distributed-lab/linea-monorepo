4d8c67432e720a034e90fec1b6b008ae3fe9eeff
Anton Levochko <hykakdela@gmail.com>
2025-06-26 16:10:23 +0300

    Update fetchers arithmetization according to the smaller field

--- prover/zkevm/prover/publicInput/fetchers_arithmetization/rlp_txn_fetcher.go
+++ prover/zkevm/prover/publicInput/fetchers_arithmetization/rlp_txn_fetcher.go
@@ -1,6 +1,7 @@
 package fetchers_arithmetization
 
 import (
+	"fmt"
 	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/protocol/column"
@@ -9,60 +10,76 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/query"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	sym "github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/consensys/linea-monorepo/prover/zkevm/prover/common"
 	commonconstraints "github.com/consensys/linea-monorepo/prover/zkevm/prover/common/common_constraints"
 	arith "github.com/consensys/linea-monorepo/prover/zkevm/prover/publicInput/arith_struct"
 	util "github.com/consensys/linea-monorepo/prover/zkevm/prover/publicInput/utilities"
 )
 
 type RlpTxnFetcher struct {
-	AbsTxNum, AbsTxNumMax ifaces.Column // Absolute number of the transaction (starts from 1 and acts as an Active Filter), and the maximum number of transactions
-	Limb                  ifaces.Column
+	// Absolute number of the transaction (starts from 1 and acts as an Active Filter), and the maximum number of
+	// transactions
+	AbsTxNum, AbsTxNumMax ifaces.Column
+	Limbs                 [common.NbLimbU128]ifaces.Column
 	NBytes                ifaces.Column
-	FilterFetched         ifaces.Column // isActive filter pattern that lights up on the area containing relevant data
-	EndOfRlpSegment       ifaces.Column // lights up on active rows i for which AbsTxNum[i]!=AbsTxNum[i+1]
+	// isActive filter pattern that lights up on the area containing relevant data
+	FilterFetched ifaces.Column
+	// lights up on active rows i for which AbsTxNum[i]!=AbsTxNum[i+1]
+	EndOfRlpSegment ifaces.Column
 	// prover action selectors
-	SelectorDiffAbsTxId        ifaces.Column // used to compute EndOfRlpSegment, lights up on active rows i for which AbsTxNum[i]!=AbsTxNum[i+1]
+	// used to compute EndOfRlpSegment, lights up on active rows i for which AbsTxNum[i]!=AbsTxNum[i+1]
+	SelectorDiffAbsTxId        ifaces.Column
 	ComputeSelectorDiffAbsTxId wizard.ProverAction
-	// chainID
-	ChainID       ifaces.Column // a size 1 column used to fetch the ChainID. The implementation is currently unaligned with respect to the number of limbs.
-	NBytesChainID ifaces.Column // a size 1 column used to fetch the number of bytes of the ChainID limb data
+	// chainID a size 1 column used to fetch the ChainID. The implementation is currently unaligned with respect to the
+	// number of limbs.
+	ChainID [common.NbLimbU128]ifaces.Column
+	// a size 1 column used to fetch the number of bytes of the ChainID limb data
+	NBytesChainID ifaces.Column
 }
 
 func NewRlpTxnFetcher(comp *wizard.CompiledIOP, name string, rt *arith.RlpTxn) RlpTxnFetcher {
-	size := rt.Limb.Size()
+	size := rt.Limbs[0].Size()
 	res := RlpTxnFetcher{
 		AbsTxNum:        util.CreateCol(name, "ABS_TX_NUM", size, comp),
 		AbsTxNumMax:     util.CreateCol(name, "ABS_TX_NUM_MAX", size, comp),
-		Limb:            util.CreateCol(name, "LIMB", size, comp),
 		NBytes:          util.CreateCol(name, "NBYTES", size, comp),
 		FilterFetched:   util.CreateCol(name, "FILTER_FETCHED", size, comp),
 		EndOfRlpSegment: util.CreateCol(name, "END_OF_RLP_SEGMENT", size, comp),
-		ChainID:         util.CreateCol(name, "CHAIN_ID", size, comp),
 		NBytesChainID:   util.CreateCol(name, "N_BYTES_CHAIN_ID", size, comp),
 	}
+
+	for i := range res.Limbs {
+		res.Limbs[i] = util.CreateCol(name, fmt.Sprintf("LIMB_%d", i), size, comp)
+		res.ChainID[i] = util.CreateCol(name, fmt.Sprintf("CHAIN_ID_%d", i), size, comp)
+	}
+
 	return res
 }
 
 // ConstrainChainID defines constraints for both ChainID and NBytesChainID columns.
 func ConstrainChainID(comp *wizard.CompiledIOP, fetcher *RlpTxnFetcher, name string, rlpTxnArith *arith.RlpTxn) {
 
-	commonconstraints.MustBeConstant(comp, fetcher.ChainID)
+	for i := range fetcher.ChainID {
+		commonconstraints.MustBeConstant(comp, fetcher.ChainID[i])
+	}
 	commonconstraints.MustBeConstant(comp, fetcher.NBytesChainID)
 
 	// constraint for the ChainID column
-	comp.InsertGlobal(
-		0,
-		ifaces.QueryIDf("%s_CHAIN_ID_GLOBAL_CONSTRAINT", name),
-		sym.Mul(
-			rlpTxnArith.IsPhaseChainID, // must be 1 to fetch ChainID
-			rlpTxnArith.Done,           // must be 1 to fetch the ChainID
-			rlpTxnArith.ToHashByProver,
-			sym.Sub(
-				rlpTxnArith.Limb,
-				fetcher.ChainID,
+	for i := range rlpTxnArith.Limbs {
+		comp.InsertGlobal(
+			0,
+			ifaces.QueryIDf("%s_CHAIN_ID_GLOBAL_CONSTRAINT_%d", name, i),
+			sym.Mul(
+				rlpTxnArith.IsPhaseChainID, // must be 1 to fetch ChainID
+				rlpTxnArith.Done,           // must be 1 to fetch the ChainID
+				rlpTxnArith.ToHashByProver,
+				sym.Sub(
+					rlpTxnArith.Limbs[i],
+					fetcher.ChainID[i],
+				),
 			),
-		),
-	)
+		)
+	}
 	// Constraint for the NBytesChainID column
 	comp.InsertGlobal(
 		0,
@@ -131,19 +148,17 @@ func DefineRlpTxnFetcher(comp *wizard.CompiledIOP, fetcher *RlpTxnFetcher, name 
 	)
 
 	// the table with the data we fetch from the arithmetization columns RlpTxn
-	fetcherTable := []ifaces.Column{
+	fetcherTable := append(fetcher.Limbs[:],
 		fetcher.AbsTxNum,
 		fetcher.AbsTxNumMax,
-		fetcher.Limb,
 		fetcher.NBytes,
-	}
+	)
 	// the RlpTxn we extract timestamp data from, and which we will use to check for consistency
-	arithTable := []ifaces.Column{
+	arithTable := append(rlpTxnArith.Limbs[:],
 		rlpTxnArith.AbsTxNum,
 		rlpTxnArith.AbsTxNumMax,
-		rlpTxnArith.Limb,
 		rlpTxnArith.NBytes,
-	}
+	)
 
 	// a projection query to check that the timestamp data is fetched correctly
 	comp.InsertProjection(
@@ -157,33 +172,44 @@ func DefineRlpTxnFetcher(comp *wizard.CompiledIOP, fetcher *RlpTxnFetcher, name 
 
 func AssignRlpTxnFetcher(run *wizard.ProverRuntime, fetcher *RlpTxnFetcher, rlpTxnArith *arith.RlpTxn) {
 
-	absTxNum := make([]field.Element, rlpTxnArith.Limb.Size())
-	absTxNumMax := make([]field.Element, rlpTxnArith.Limb.Size())
-	limb := make([]field.Element, rlpTxnArith.Limb.Size())
-	nBytes := make([]field.Element, rlpTxnArith.Limb.Size())
-	filterFetched := make([]field.Element, rlpTxnArith.Limb.Size())
-	endOfRlpSegment := make([]field.Element, rlpTxnArith.Limb.Size())
+	size := rlpTxnArith.Limbs[0].Size()
 
-	var chainID, nBytesChainID field.Element
+	absTxNum := make([]field.Element, size)
+	absTxNumMax := make([]field.Element, size)
+	limbs := make([][]field.Element, len(rlpTxnArith.Limbs))
+	nBytes := make([]field.Element, size)
+	filterFetched := make([]field.Element, size)
+	endOfRlpSegment := make([]field.Element, size)
+
+	for i := range limbs {
+		limbs[i] = make([]field.Element, size)
+	}
+
+	chainID := make([]field.Element, len(rlpTxnArith.Limbs))
+	var nBytesChainID field.Element
 
 	// counter is used to populate filter.Data and will increment every time we find a new timestamp
 	counter := 0
 
-	for i := 0; i < rlpTxnArith.Limb.Size(); i++ {
+	for i := 0; i < size; i++ {
 		toHashByProver := rlpTxnArith.ToHashByProver.GetColAssignmentAt(run, i)
 		// process the RLP limb, by inspecting AbsTxNum, AbsTxNumMax, Limb, NBytes
 		// and populating a row of the fetcher with these values.
 		if toHashByProver.IsOne() {
 			arithAbsTxNum := rlpTxnArith.AbsTxNum.GetColAssignmentAt(run, i)
 			arithAbsTxNumMax := rlpTxnArith.AbsTxNumMax.GetColAssignmentAt(run, i)
-			arithLimb := rlpTxnArith.Limb.GetColAssignmentAt(run, i)
 			arithNBytes := rlpTxnArith.NBytes.GetColAssignmentAt(run, i)
 
 			absTxNum[counter].Set(&arithAbsTxNum)
 			absTxNumMax[counter].Set(&arithAbsTxNumMax)
-			limb[counter].Set(&arithLimb)
 			nBytes[counter].Set(&arithNBytes)
 			filterFetched[counter].SetOne()
+
+			for j := range rlpTxnArith.Limbs {
+				arithLimb := rlpTxnArith.Limbs[j].GetColAssignmentAt(run, i)
+				limbs[j][counter].Set(&arithLimb)
+			}
+
 			counter++
 		}
 		// check if we have the ChainID
@@ -191,15 +217,18 @@ func AssignRlpTxnFetcher(run *wizard.ProverRuntime, fetcher *RlpTxnFetcher, rlpT
 		isPhaseChainID := rlpTxnArith.IsPhaseChainID.GetColAssignmentAt(run, i)
 		if done.IsOne() && isPhaseChainID.IsOne() && toHashByProver.IsOne() {
 			// fetch the ChainID from the limb column
-			fetchedValue := rlpTxnArith.Limb.GetColAssignmentAt(run, i)
-			chainID.Set(&fetchedValue)
+			for j := range rlpTxnArith.Limbs {
+				fetchedValue := rlpTxnArith.Limbs[j].GetColAssignmentAt(run, i)
+				chainID[j].Set(&fetchedValue)
+			}
+
 			// fetch the number of bytes for the ChainID
 			fetchedNBytes := rlpTxnArith.NBytes.GetColAssignmentAt(run, i)
 			nBytesChainID.Set(&fetchedNBytes)
 		}
 	}
 
-	for i := 0; i < rlpTxnArith.Limb.Size()-1; i++ {
+	for i := 0; i < size-1; i++ {
 		if filterFetched[i].IsOne() {
 			// only set end of segments in the active area
 			if !absTxNum[i].Equal(&absTxNum[i+1]) {
@@ -209,15 +238,17 @@ func AssignRlpTxnFetcher(run *wizard.ProverRuntime, fetcher *RlpTxnFetcher, rlpT
 	}
 
 	// assign the fetcher columns
-	size := fetcher.AbsTxNum.Size()
 	run.AssignColumn(fetcher.AbsTxNum.GetColID(), smartvectors.RightZeroPadded(absTxNum[:counter], size))
 	run.AssignColumn(fetcher.AbsTxNumMax.GetColID(), smartvectors.RightZeroPadded(absTxNumMax[:counter], size))
-	run.AssignColumn(fetcher.Limb.GetColID(), smartvectors.RightZeroPadded(limb[:counter], size))
 	run.AssignColumn(fetcher.NBytes.GetColID(), smartvectors.RightZeroPadded(nBytes[:counter], size))
 	run.AssignColumn(fetcher.FilterFetched.GetColID(), smartvectors.RightZeroPadded(filterFetched[:counter], size))
 	run.AssignColumn(fetcher.EndOfRlpSegment.GetColID(), smartvectors.NewRegular(endOfRlpSegment), wizard.DisableAssignmentSizeReduction)
-	run.AssignColumn(fetcher.ChainID.GetColID(), smartvectors.NewConstant(chainID, size))
 	run.AssignColumn(fetcher.NBytesChainID.GetColID(), smartvectors.NewConstant(nBytesChainID, size))
+
+	for i := range rlpTxnArith.Limbs {
+		run.AssignColumn(fetcher.Limbs[i].GetColID(), smartvectors.RightZeroPadded(limbs[i][:counter], size))
+		run.AssignColumn(fetcher.ChainID[i].GetColID(), smartvectors.NewConstant(chainID[i], size))
+	}
 
 	fetcher.ComputeSelectorDiffAbsTxId.Run(run)
 }
